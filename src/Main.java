ed9816acc89eff5afbb166f48ae9a5cd2222b874
Teo Lind <teo.l.lind@gmail.com>
2024-09-12 13:43:42 +0200

    more menu

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("1. Add animal\n 2. View Animals\n 3. Show animal details\n 4. Remove animal\n 5. Exit ");
         ArrayList<String> animals = new ArrayList<>();
+        System.out.println(animals);
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Select an option:");
@@ -12,12 +13,15 @@ public class Main {
 
         switch (choice) {
             case 1:
-                //code
+                //code add
             case 2:
-                //cpde
+                //cpde read list
             case 3:
-                //code
+                //code read details of chosen animal
             case 4:
+                //remove animal
+            case 5:
+                //exit message
 
         }
 
