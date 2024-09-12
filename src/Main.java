d94958183258b2178fbcda797782c8136ae67918
Teo Lind <teo.l.lind@gmail.com>
2024-09-12 13:36:42 +0200

    added switch menue

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("1. Add animal\n 2. View Animals\n 3. Show animal details\n 4. Remove animal\n 5. Exit ");
-    ArrayList<String> animals = new ArrayList<>();
+        ArrayList<String> animals = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Select an option:");
+        int choice = sc.nextInt();
+
+        switch (choice) {
+            case 1:
+                //code
+            case 2:
+                //cpde
+            case 3:
+                //code
+            case 4:
+
+        }
 
     }
 }
