1ad80fde9466a080bdf31db8f222a04b8bbc775e
Simon Rintala <siimon_7@icloud.com>
2024-09-12 13:22:42 +0200

    Added menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("1. Add animal\n 2. View Animals\n 3. Show animal details\n 4. Remove animal\n 5. Exit ");
     }
 }
