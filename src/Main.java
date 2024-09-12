5135f08a518de68a6004dd94e54243816f938f4f
Teo Lind <teo.l.lind@gmail.com>
2024-09-12 13:59:16 +0200

    added a second array to hold animal descriptions

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("1. Add animal\n 2. View Animals\n 3. Show animal details\n 4. Remove animal\n 5. Exit ");
         ArrayList<String> animals = new ArrayList<>();
+        ArrayList<String> animalDescriptions = new ArrayList<>();
         System.out.println(animals);
         Scanner sc = new Scanner(System.in);
 
@@ -16,9 +17,12 @@ public class Main {
                 System.out.print("What animal do you want to add: ");
                 String animal = sc.nextLine();
                 animals.add(animal);
+                System.out.print("Describe the animal further: ");
+                String description = sc.nextLine();
+                animalDescriptions.add(description);
                 System.out.println("\nAnimal has been added to the zoo.");
             case 2:
-                //cpde read list
+                //code read list
             case 3:
                 //code read details of chosen animal
             case 4:
